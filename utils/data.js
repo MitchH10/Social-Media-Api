f9a223ac2fe4500ab7df1ad1427d6dc2adaa818a
MitchH10 <mjh10@live.com>
2023-02-28 21:29:37 -0600

    added seed data

---
+++ utils/data.js
@@ -0,0 +1,21 @@
+const users = [
+  {
+    username: "calebcarnett",
+    email: "caleb@gmail.com",
+  },
+];
+
+const thoughts = [
+  {
+    thoughtText: "I love my Cat",
+    username: "calebcarnett",
+    reactions: [
+      {
+        reactionBody: "He's adorable",
+        username: "bobby",
+      },
+    ],
+  },
+];
+
+module.exports = { users, thoughts };
